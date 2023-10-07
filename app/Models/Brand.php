15400b0ee35235a1f99018db7471eb4ed32c16ce
OjekPantai <elvien.purnawan01@gmail.com>
2023-10-07 17:12:21 +0700

    first commit

---
+++ app/Models/Brand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+    public function getCreatedAtAttribute()
+    {
+        return \Carbon\Carbon::parse($this->attributes['created_at'])->translatedFormat('l, d F Y');
+    }
+
+    public function getUpdatedAtAttribute()
+    {
+        return \Carbon\Carbon::parse($this->attributes['updated_at'])->diffForHumans();
+    }
+
+
+}
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route untuk CRUD Brand
+Route::middleware(['auth','verified'])->group(function(){
+    // menampilkan data
+    Route::get('/brands',[BrandController::class,'index'])->name('brand.index');
+    // menampilkan form tambah data
+    Route::get('/brands/create',[BrandController::class, 'create'])->name('brand.create');
+    // menampilkan data ke database
+    Route::post('/brands',[BrandController::class,'store'])->name('brand.store');
+    // menampilkan form edit data
+    Route::get('/brands/{id}/edit',[BrandController::class, 'edit'])->name('brand.edit');
+    // menampilkan data ke database
+    Route::put('/brands/{id}',[BrandController::class, 'update'])->name('brand.update');
+    // menghapud data dari dataabase
+    Route::delete('/brands/{id}',[BrandController::class, 'destroy'])->name('brand.destroy');
+}
+);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+require __DIR__.'/auth.php';
